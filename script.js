be9a7a3d3df6862ba417adca46a60727543c755f
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-03-12 12:54:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const columns = document.querySelectorAll(".column");
-
-document.addEventListener("dragstart", (element)=>{
-    element.target.classList.add("dragging");
 });
 
 document.addEventListener("dragend", (element)=>{
@@ -38,18 +34,23 @@ function getNewPosition(column, posY){
 }
 
 createNewCard = () => {
+
+    const card = document.querySelector('.columnToDo')
+    const creationCard = document.querySelector('.newCard')
+
+    console.log(card)
     
-    let initialHtml=`<div class="item" draggable="true">
-    <p>Example Task<br><br>If you want to change the status of task 
-    grab and drop to others columns next to.</p>
-</div>
-<div class="item" draggable="true">
-    <p>New Task<br><br></p>
-</div>          
-<div class="item" draggable="true">
-    <p>Create a new Task</p>
-    <br>
-    <button onClick={createNewCard}> Click here</button>
-</div> `
+    /*const initialHtml=`<div class="item" draggable="true">
+    <p>Write here<br><br></p>
+    </div>`
+
+    
+    let newCard=initialHtml+card.outerHTML*/
+
+
+
+    console.log(card + newCard)
+
+    //card.innerHTML = newCard
 
 }
